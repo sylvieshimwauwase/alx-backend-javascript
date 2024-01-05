778ee14339ae31affbbcd1f63b388cb352b0924a
sylvieshimwauwase <slvshimwa@gmail.com>
2024-01-05 23:33:03 +0200

    ES6_classes javascript

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,16 +1,16 @@
-import { ThemeConsumer } from 'styled-components';
-
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
-  valueOf() {
-    return this._size;
-  }
-
-  toString() {
-    return this._location;
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+    if (hint === 'number') {
+      return this._size;
+    }
+    return null;
   }
 }
