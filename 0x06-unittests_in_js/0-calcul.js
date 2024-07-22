c2d1cd4c96717e934a687213d5dcb2078b212589
sylvieshimwauwase <slvshimwa@gmail.com>
2024-07-22 13:08:35 +0200

    working on unittest files

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+const calculateNumber = (a, b) => {
+   const aRound = Math.round(a);
+   const bRound = Math.round(b);
+ 
+   return aRound + bRound;
+ };
+ 
+ module.exports = calculateNumber;
+ 
---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+const getPaymentTokenFromAPI = (success) => {
+   if (success === true) {
+     return Promise.resolve({ data: 'Successful response from the API' });
+   }
+   return null;
+ };
+ 
+ module.exports = getPaymentTokenFromAPI;
+ 
